e1df9a2926bc20f62a9e30716a547cf92f4df7f8
jiwon <zest1923@gmail.com>
2021-11-08 18:46:59 +0900

    coin tracker

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { useState, useEffect } from "react"
 
 function App() {
+
+  const [loading, setLoading] = useState(true)
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((respone) => respone.json())
+      .then((json) => {
+        setCoins(json); // coin을 setcoin에 삽입
+        setLoading(false); // loading이 끝났으므로 setloading 값 변경
+      }) // json = coin
+  }, []) // API 호출은 1번만! 따라서, 두 번째 인자는 빈 리스트
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>The coins! {loading ? null : `(${coins.length})`} </h1>
+      {loading ? <strong>Loading...</strong> : <ul>
+        {coins.map((coin) =>
+          <li>{coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD</li>
+        )}
+      </ul>}
+
+
     </div>
   );
 }
